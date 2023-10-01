6a9df1be3fa4b15bd7fa42a68235b153141a76dd
Supanut1911 <ynotmela@gmail.com>
2023-10-01 17:12:01 +0700

    practices simple http call

---
+++ cmd/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Person struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("server up")
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		return c.SendString("this post")
+	})
+
+	app.Get("/hello/:name", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		return c.SendString("name:" + name)
+	})
+
+	app.Get("/hello/:name/:sname", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		sname := c.Params("sname")
+		return c.SendString("name:" + name + "sname" + sname)
+	})
+
+	app.Get("/num/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+		if err != nil {
+			return fiber.ErrBadRequest
+		}
+		return c.SendString(fmt.Sprintf("ID = %v", id))
+	})
+
+	app.Get("/query", func(c *fiber.Ctx) error {
+		name := c.Query("name")
+		sname := c.Query("sname")
+		return c.SendString("name:" + name + " " + sname)
+	})
+
+	app.Get("/query2", func(c *fiber.Ctx) error {
+		person := Person{}
+		c.QueryParser(&person)
+		return c.JSON(person)
+	})
+
+	app.Listen(":3000")
+}
